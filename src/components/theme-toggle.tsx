cc8801aef73a57d182cc9582864fc116f98c20ef
MUSONDA SALIMU (Muzo) <Musondasalim@gmail.com>
2025-08-24 23:11:41 +0000

    theme changer is not working properly, fix any errors

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -20,8 +20,8 @@ export function ThemeToggle() {
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark-neural-networks:scale-0" />
-          <Cpu className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark-neural-networks:rotate-0 dark-neural-networks:scale-100" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 [.dark-neural-networks_&]:scale-0" />
+          <Cpu className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all [.dark-neural-networks_&]:rotate-0 [.dark-neural-networks_&]:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
