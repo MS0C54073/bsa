40c215263279cec5b5f0a1fdf6e008ac8ad9a52c
MUSONDA SALIMU (Muzo) <Musondasalim@gmail.com>
2025-08-24 22:59:27 +0000

    the theme changes are not working properly, its should show circuit anim

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -13,14 +13,14 @@ import {
 } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, theme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark-neural-networks:-rotate-90 dark-neural-networks:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark-neural-networks:scale-0" />
           <Cpu className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark-neural-networks:rotate-0 dark-neural-networks:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
